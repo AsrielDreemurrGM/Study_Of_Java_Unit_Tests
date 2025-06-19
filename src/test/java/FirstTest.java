c72b0222bd4f7f0aba810c25e19a57f09cf706a9
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-19 11:12:34 -0300

    Created FirstTest Class
    - Learned How To Add The JUnit 5 Library Into Project;
    - Learned How Project Structure With Tests Is ("src/test/java" And "src/main/java");
    - Created "assertEquals" And "assertNotEquals" Simple Tests;

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
+ * @since Jun 19, 2025
+ */
+public class FirstTest {
+
+	@Test
+	public void test() {
+		String name = "Eduardo";
+		Assert.assertEquals("Eduardo", name);
+	}
+
+	@Test
+	public void testNotEquals() {
+		String name = "Eduardo";
+		Assert.assertNotEquals("Eduardo1", name);
+	}
+}
